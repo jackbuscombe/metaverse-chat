270defc93a63432331bf365d9bbfddbb48723dfd
Jack Buscombe <jackbuscombe94@gmail.com>
2022-04-22 21:14:40 +1000

    Edit to title of login page

--- components/Login.js
+++ components/Login.js
@@ -6,13 +6,15 @@ function Login() {
 
 	return (
 		<div className="bg-black relative">
-			<h1>I Am the Login</h1>
-			<div className="flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4">
+			<div className="flex flex-col absolute z-50 h-5/6 w-full items-center justify-center space-y-6">
+				<div className="p-6 bg-fuchsia-700 bg-opacity-70">
+					<h1 className="text-5xl text-white font-extrabold font-mono">Welcome to the Metaverse</h1>
+				</div>
 				<div className="h-48 w-48 relative bg-black p-4 rounded-full bg-opacity-50 mb-6">
 					<Image className="object-cover rounded-full" src={`https://avatars.dicebear.com/api/pixel-art/${(Math.random() + 1).toString(36).substring(7)}.svg`} layout="fill" />
 				</div>
-				<button onClick={authenticate} className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">
-					Login to the metaverse
+				<button onClick={authenticate} className="bg-yellow-300 rounded-lg p-5 px-12 font-bold animate-pulse">
+					Click to Enter
 				</button>
 			</div>
 
